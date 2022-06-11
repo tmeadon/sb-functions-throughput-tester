99f7346cca570a4b9cb1ef02498be228394a1a28
Tom Meadon <tom@meadon.me>
2022-06-11 13:04:39 +0100

    remove dashes from storage acc name

--- infra/main.tf
+++ infra/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_servicebus_queue" "sb_out" {
 }
 
 resource "azurerm_storage_account" "stg" {
-  name                = local.unique_name
+  name                = replace(local.unique_name, "-", "")
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
